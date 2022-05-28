b84e065112a1eb471f0c66e3c746cb0a0c865a2c
shinkay0 <101357187+shinkay0@users.noreply.github.com>
2022-05-28 19:43:20 +0200

    wip

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/shinkay0/goflix
 go 1.18
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/mattn/go-sqlite3 v1.14.13
--- routes_auth.go
+++ routes_auth.go
@@ -2,11 +2,74 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func (s *server) handleIndex() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to Goflix")
+		fmt.Fprintf(w, "Welcome to Goflix!")
+	}
+}
+
+func (s *server) handleTokenCreate() http.HandlerFunc {
+	type request struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	type response struct {
+		Token string `json:"token"`
+	}
+
+	type responseError struct {
+		Error string `json:"error"`
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := request{}
+		err := s.decode(w, r, &req)
+		if err != nil {
+			msg := fmt.Sprintf("Cannot parse login body. err=%v", err)
+			log.Println(msg)
+			s.respond(w, r, nil, http.StatusBadRequest)
+			s.respond(w, r, responseError{
+				Error: msg,
+			}, http.StatusUnauthorized)
+			return
+		}
+		// database version
+
+		// hard coded version
+		//if req.Username != "golang" || req.Password != "rocks" {
+		if req.Username != "golang" || req.Password != "rocks" {
+			s.respond(w, r, responseError{
+				Error: "Invalid credentials",
+			}, http.StatusUnauthorized)
+			return
+		}
+
+		// ? GENERATE TOKEN
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": req.Username,
+			"exp":      time.Now().Add(time.Hour * time.Duration(1)).Unix(),
+			"iat":      time.Now().Unix(),
+		})
+
+		tokenStr, err := token.SignedString([]byte(JWT_APP_KEY))
+
+		if err != nil {
+			msg := fmt.Sprintf("Cannot generate JWT. error: %v", err)
+			s.respond(w, r, responseError{
+				Error: msg,
+			}, http.StatusInternalServerError)
+			return
+		}
+
+		s.respond(w, r, response{
+			Token: tokenStr,
+		}, http.StatusOK)
 	}
 }
