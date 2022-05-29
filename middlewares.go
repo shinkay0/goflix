085e81947c9aa343ed9eddd01aa45ffe7d032cbd
shinkay0 <101357187+shinkay0@users.noreply.github.com>
2022-05-29 02:59:32 +0200

    WIP

--- middlewares.go
+++ middlewares.go
@@ -3,11 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func logRequestMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("[%v] %v", r.Method, r.RequestURI)
 		next.ServeHTTP(w, r)
+	}
+}
+
+func (s *server) loggedOnly(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		j := jwtmiddleware.New(jwtmiddleware.Options{
+			ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+				return []byte(JWT_APP_KEY),
+			}
+			SigningMethodHS256,
+		})
+
+		j.HandlerWithNext(w, r, next)
 	}
 }
--- routes.go
+++ routes.go
@@ -3,7 +3,7 @@ package main
 func (s *server) routes() {
 	s.router.HandleFunc("/", s.handleIndex()).Methods("GET")
 	s.router.HandleFunc("/api/token", s.handleTokenCreate()).Methods("POST")
-	s.router.HandleFunc("/api/movies/{id:[0-9]+}", s.handleMovieDetail()).Methods("GET")
-	s.router.HandleFunc("/api/movies/", s.handleMovieList()).Methods("GET")
-	s.router.HandleFunc("/api/movies/", s.handleMovieCreate()).Methods("POST")
+	s.router.HandleFunc("/api/movies/{id:[0-9]+}", s.loggedOnly(s.handleMovieDetail())).Methods("GET")
+	s.router.HandleFunc("/api/movies/", s.loggedOnly(s.handleMovieList())).Methods("GET")
+	s.router.HandleFunc("/api/movies/", s.loggedOnly(s.handleMovieCreate())).Methods("POST")
 }
